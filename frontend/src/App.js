2cdfdf28e8e811eb8d3690aea21bee900478b644
Shashi Vinod <it21288326@my.sliit.lk>
2024-11-29 21:06:57 +0530

    "Updated package-lock.json and package.json files in backend and frontend directories; added 'backend' and 'frontend' dependencies; removed logo192.png and logo512.png files; modified App.js file in frontend/src directory."

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
-// src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ReactionTimeGame from './component/it21288326/ReactiontimeGame';
+import QuestionnaireForm from './component/it21288326/QuestionnaireForm';
+import SignIn from './component/it21288326/SignIn';
+import SignUp from './component/it21288326/SignUp';
+import Profile from './component/it21288326/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <ReactionTimeGame />
-    </div>
+    <Router>
+      
+      <div className="App">
+
+        {/* Navigation Links
+        <nav>
+          <ul>
+            <li><Link to="/reaction-time-game">Reaction Time Game</Link></li>
+            <li><Link to="/questionnaire-form">Questionnaire Form</Link></li>
+          </ul>
+        </nav> */}
+
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/reaction-time-game" element={<ReactionTimeGame />} />
+          <Route path="/questionnaire-form" element={<QuestionnaireForm />} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
