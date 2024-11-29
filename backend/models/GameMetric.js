e26b136e90a562e4162e2397d7b0b08da8b505b5
Shashi Vinod <it21288326@my.sliit.lk>
2024-11-29 21:06:57 +0530

    "Updated package-lock.json and package.json files in backend and frontend directories; added 'backend' and 'frontend' dependencies; removed logo192.png and logo512.png files; modified App.js file in frontend/src directory."

--- backend/models/GameMetric.js
+++ backend/models/GameMetric.js
@@ -2,12 +2,18 @@
 import mongoose from 'mongoose';
 
 const gameMetricSchema = new mongoose.Schema({
+  childId: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Child', // Reference to the Child model
+    required: true 
+  },
   reactionTimes: { type: [Number], required: true },
   averageReactionTime: { type: Number, required: true },
   correctStreak: { type: Number, required: true },
   prematureClicks: { type: Number, required: true },
   missedStars: { type: Number, required: true },
   score: { type: Number, required: true },
+
   createdAt: { type: Date, default: Date.now }
 });
 
